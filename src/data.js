7f64b49c717891418594d00561536c4a95f12f86
Moore <mooreart95@gmail.com>
2022-06-30 08:51:46 -0600

    About Me and first Project

---
+++ src/data.js
@@ -0,0 +1,10 @@
+export const projects = [
+    {
+      title: "Try My Trivia",
+      subtitle: "Javascript, HTML, and CSS",
+      description:
+        "A quick trivia game for when you have some time and want to relax.",
+      image: "",
+      link: "https://micahmoore95.github.io/TryMyTrivia/",
+    }
+]
