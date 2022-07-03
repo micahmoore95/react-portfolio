d6bf067c51d3ac4ae0a0711535539efea6d6733d
Moore <mooreart95@gmail.com>
2022-07-03 14:18:17 -0600

    project tests

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,26 @@
-function Navbar() {
+import React from "react";
 
+
+function Navbar() {
+    const { currentPage, setCurrentPage } = props;
+
+  return (
+    <header className="flex-row px-1">
+      <h2>
+          Micah Moore
+      </h2>
+      <nav>
+        <ul className="flex-row">
+          <li className={currentPage === "about" ? "mx-2 navActive" : "mx-2"}>
+            <span onClick={() => setCurrentPage("About")}>About Me</span>
+          </li>
+          <li className={currentPage === "myprojects" ? "mx-2 navActive" : "mx-2"}>
+            <span onClick={() => setCurrentPage("MyProjects")}>Projects</span>
+          </li>
+      </ul>
+    </nav>
+    </header >
+  );
 };
 
 export default Navbar();
