2cc3628e209759c1531279dee54dd4c56bd8d9bc
Moore <mooreart95@gmail.com>
2022-07-03 21:04:32 -0600

    README

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,13 +11,13 @@ function Contact() {
                   <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                     EMAIL
                   </h2>
-                  <a className="text-indigo-400 leading-relaxed">
+                  <a href='moorart95@gmail.com' className="text-indigo-400 leading-relaxed">
                     mooreart95@gmail.com
                   </a>
                 </div>
               </div>
             </div>
-            <form
+            {/* <form
               netlify
               name="contact"
               className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
@@ -63,7 +63,7 @@ function Contact() {
                 className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
                 Submit
               </button>
-            </form>
+            </form> */}
           </div>
         </section>
       );
--- src/data.js
+++ src/data.js
@@ -1,11 +1,11 @@
 export const projectList = [
     {
-      title: "Try My Trivia",
-      subtitle: "Javascript, HTML, and CSS",
+      title: "Spoiled Milk",
+      subtitle: "React, Javascript, HTML, and CSS",
       description:
-        "A quick trivia game for when you have some time and want to relax.",
+        "A MERN Stack application that allows you to keep track of what foods you have in your fridge so that things don't expire.",
       image: "",
-      link: "https://micahmoore95.github.io/TryMyTrivia/",
+      link: "https://micahmoore95.github.io/react-portfolio/",
     },
     {
       title: "Try My Trivia",
